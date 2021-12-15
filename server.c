2a91ed94ed776154396be78a04fc82a997cf9bfa
Ansis Maleckis <maleckisansis@gmail.com>
2021-12-15 16:18:09 +0200

    improved server

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: amalecki <amalecki@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 16:15:32 by amalecki          #+#    #+#             */
-/*   Updated: 2021/12/15 11:22:02 by amalecki         ###   ########.fr       */
+/*   Updated: 2021/12/15 16:17:13 by amalecki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,28 +21,25 @@ void	s_sig_handler(int signum)
 	s_flag[1]++;
 }
 
-static void	reset(void)
-{
-		s_flag[0] = 0;
-		s_flag[1] = 0;
-}
-
 static void	find_client(int *client)
 {
-	int a;
-
 	while (s_flag[0] < 999999)
 	{
-		reset();
-		a = get_data(31);
-		if (a > 0 && a < 777777)
-			*client = a;
+		s_flag[0] = 0;
+		s_flag[1] = 0;
+		get_data(31);
+		if (s_flag[0] > 0 && s_flag[0] < 777777)
+			*client = s_flag[0];
 		kill(*client, SIGUSR1);
-	}	
+	}
+	s_flag[0] = 0;
+	s_flag[1] = 0;
 }
 
 int	get_data(int bits)
 {
+	s_flag[0] = 0;
+	s_flag[1] = 0;
 	pause();
 	while (bits)
 	{
@@ -52,67 +49,35 @@ int	get_data(int bits)
 	return (s_flag[0]);
 }
 
+void	init_sigaction(struct sigaction *s_action, void (*sig_handler)(int))
+{
+	sigemptyset(&s_action->sa_mask);
+	sigaddset(&s_action->sa_mask, SIGINT);
+	s_action->sa_flags = 0;
+	s_action->sa_handler = sig_handler;
+	sigaction(SIGUSR1, s_action, NULL);
+	sigaction(SIGUSR2, s_action, NULL);
+}
+
 int	main(void)
 {
 	pid_t				server;
 	pid_t				client;
 	struct sigaction	s_action;
-	char p;
 
 	init_sigaction(&s_action, s_sig_handler);
 	server = getpid();
 	ft_printf("%d\n", server);
-	while(true)
+	while (true)
 	{
 		find_client(&client);
-		printf("s_flag: %d\n", (int)s_flag[0]);
-		ft_printf("client pid: %d\n", client);
-		reset();
-		p = get_data(7);
-		while (p)
+		get_data(7);
+		while (s_flag[0])
 		{
-			write(1, &p, 1);
-			reset();
+			write(1, &s_flag[0], 1);
 			kill(client, SIGUSR2);
-			p = get_data(7);
+			get_data(7);
 		}
 		kill(client, SIGUSR1);
 	}
-	}
-
-	/*
-Pseudocode for server infinite loop
-
-char *symbol;
-while(true)
-{
-	while(!client)
-	{
-		get_pid()
-		client = (int)s_flag;
-		confirm_received(flag);
-	}
-	get_signal();
-	while (signal not valid)
-	{
-		confirm_error;
-		get_signal();
-	}
-	confirm_received;
-	if(s_flag > message terminated)
-	{
-		confirm_received();
-		client = 0;
-		s_flag = 0;
-		continue;
-	}
-	else if(s_flag == message)
-	{
-		symbol = (char *)&s_flag;
-		printf("%c", symbol);
-		s_flag = 0;
-		confirm_received();
-	}
 }
-
-*/
--- server_utils.c
+++ server_utils.c
@@ -6,20 +6,10 @@
 /*   By: amalecki <amalecki@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/12 08:45:28 by amalecki          #+#    #+#             */
-/*   Updated: 2021/12/13 17:31:34 by amalecki         ###   ########.fr       */
+/*   Updated: 2021/12/15 16:08:24 by amalecki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
-
-void	init_sigaction(struct sigaction *s_action, void (*sig_handler)(int))
-{
-	sigemptyset(&s_action->sa_mask);
-	sigaddset(&s_action->sa_mask, SIGINT);
-	s_action->sa_flags = 0;
-	s_action->sa_handler = sig_handler;
-	sigaction(SIGUSR1, s_action, NULL);
-	sigaction(SIGUSR2, s_action, NULL);
-}
 
 
